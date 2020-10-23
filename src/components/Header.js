8345efdfa54a074f1387d57cb631e518167f6ab8
hueindahaus <huanga@student.chalmers.se>
2020-10-23 23:11:01 +0200

    Added Greensock api for animations
    Added some landing page animations in the header and scrolltrigger-animations in MainContent Component (which now is converted to a class component)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,49 @@
-import React from "react"
+import React, {useRef, useEffect} from "react"
 import BackgroundVideo from "./BackgroundVideo.js"
 import "../css/header.css"
-import facebook from "../resources/icons/facebook.svg"
-import instagram from "../resources/icons/instagram.svg"
-import twitter from "../resources/icons/twitter.svg"
+
+import {TweenMax, Power3} from "gsap"
+
+import facebookIcon from "../resources/icons/facebook.svg"
+import instagramIcon from "../resources/icons/instagram.svg"
+import twitterIcon from "../resources/icons/twitter.svg"
 
 //This is a functional component that represents the header and "landing section" of the page
+//The module uses Greensock (GSAP) library to create animations
 function Header(props){
+  //returns an object with current:null
+  let headline = useRef(null)
+  let subline = useRef(null)
+  let facebook = useRef(null)
+  let instagram = useRef(null)
+  let twitter = useRef(null)
+  let button = useRef(null)
+
+  //input animation-method which will be called like the lifecycle method componentDidMount()
+  useEffect(() => {
+    TweenMax.to(headline,1.2,{opacity:1,y: -20,ease: Power3.easeOut})
+
+    TweenMax.to(subline,1.2,{opacity:1,y: -20,ease: Power3.easeOut,delay: 0.2})
+
+    TweenMax.to(facebook,1.2,{opacity:1,y: -20,ease: Power3.easeOut,delay: 0.8})
+
+    TweenMax.to(instagram,1.2,{opacity:1,y: -20,ease: Power3.easeOut,delay: 1})
+
+    TweenMax.to(twitter,1.2,{opacity:1,y: -20,ease: Power3.easeOut,delay: 1.2})
+    
+    TweenMax.to(button,1.2,{opacity:1,y: -20,ease: Power3.easeOut,delay: 0.2})
+  }, [])
+
   return(
     <header className="header-container">
-      <h1 className="head-line">CityPop</h1>
-      <h2 className="sub-line">Population of cities all around the globe.</h2>
+      <h1 ref={element => {headline=element}} className="head-line">CityPop</h1>
+      <h2 ref={element => {subline=element}} className="sub-line">Population of cities all around the globe.</h2>
       <div className="link-box">
-        <a href="#"><img className="social-media" src={facebook}/></a>
-        <a href="#"><img className="social-media" src={instagram}/></a>
-        <a href="#"><img className="social-media" src={twitter}/></a>
+        <a href="#"><img ref={element => {facebook=element}} className="social-media" src={facebookIcon}/></a>
+        <a href="#"><img ref={element => {instagram=element}} className="social-media" src={instagramIcon}/></a>
+        <a href="#"><img ref={element => {twitter=element}} className="social-media" src={twitterIcon}/></a>
       </div>
-      <button className="get-started-button" onClick={scrollToMainSection}>Get started</button>
+      <button ref={element => {button=element}} className="get-started-button" onClick={scrollToMainSection}>Get started</button>
       <BackgroundVideo />
     </header>
   )
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,13 +1,62 @@
-import React from "react"
+import React, {Component, useEffect} from "react"
 import "../css/main-content.css"
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-function MainContent(){
-  return(
-    <main id="main" className="main-container">
+// register ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 
-    </main>
-  )
+
+
+class MainContent extends Component{
+
+  constructor(){
+    super()
+    this.state = {
+      
+    }
+  }
+
+  componentDidMount(){
+    const scrollTrigger = {
+      trigger: "#main",
+      scrub:1,
+      start:"top bottom",
+      end: "top 100px",
+      //markers: true
+  }
+
+    gsap.to('.option-button', { 
+      "margin-left": "1rem",
+      "margin-right": "1rem",
+      duration: 3,
+      scrollTrigger: scrollTrigger
+    });
+
+
+    gsap.to('.option-text', { 
+      opacity: "100%",
+      duration: 3,
+      scrollTrigger: scrollTrigger
+    });
+  }
+
+  render(){
+  
+    return(
+      <main id="main" className="main-container">
+        <div className="column">
+          <h2 className="option-text">Choose your option!</h2>
+          <div className="row">
+            <button className="option-button">Search by city</button>
+            <button className="option-button">Search by country</button>
+          </div>
+        </div>
+        
+      </main>
+    )
+  }
 }
 
 export default MainContent
